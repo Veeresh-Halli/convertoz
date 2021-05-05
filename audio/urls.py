de68efb6916d2a40ebdd452f106b85d10d96418a
Veeresh Halli <77663460+Veeresh-Halli@users.noreply.github.com>
2021-05-06 00:23:07 +0530

    added whitenoise and collectstatic

--- audio/urls.py
+++ audio/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('adminBC/', admin.site.urls),
     path('', include('audioapp.urls')),
 ]
 urlpatterns = urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
